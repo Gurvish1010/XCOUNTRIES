46d3870a89ba7d1307523eb138f7ae93d1ff9ae4
Gurvish1010 <gurvishvaja123@gmail.com>
2025-10-06 11:42:52 +0530

    xcountries initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect,useState } from 'react';
 import './App.css';
+import axios from "axios"
+
 
 function App() {
+
+  const[countries,setCountries] = useState([])
+
+  useEffect(()=> {
+    const fetchCountries = async ()=> {
+      try{
+        const res = await axios.get("https://xcountries-backend.labs.crio.do/all")
+        setCountries(res.data)
+      }catch(e){
+        console.log("fetching data error:", e)
+
+      }
+    };
+
+    fetchCountries();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {countries.map((country)=> (
+        <div key={country.abbr} className='card'>
+          <img
+          src={country.flag}
+          alt={`${country.name} Flag`}
+          className="flag"
+          />
+          <p className='countryName'>{country.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
